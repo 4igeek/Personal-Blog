2b304a8bcef45094fe60a268b4f3390bb9b98b88
VilPeas <contactvil@gmail.com>
2024-10-10 11:31:03 +0100

    First commit

--- api/functions.js
+++
@@ -1,9 +0,0 @@
-const generateVerificationCode = () => {
-    let randomCode = ''
-    for (let i = 0; i < 6; i++) {
-        randomCode += Math.floor(Math.random() * 10);
-    }
-    return randomCode;
-}
-
-module.exports.generateVerificationCode = generateVerificationCode;
--- frontend/src/Elements/Pages/Pages/AddPost.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AddPost = () => {
-    return (
-        <div>Add a new post</div>
-    )
-}
-
-export default AddPost
